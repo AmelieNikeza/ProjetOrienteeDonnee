66054521e36a842f1c3de360d58c42ea9429519d
ismaelfezzani <92721312+ismaelfezzani@users.noreply.github.com>
2023-06-08 17:24:06 +0200

    traitement des occurences trouvées dans le fichier scrappé

--- traitement.py
+++ traitement.py
@@ -22,9 +22,24 @@ with open("dataBretagne.txt", "r", encoding="utf-8") as file:
     # Parcourir les entités nommées extraites
 
     orgs = {}
+    people = {}
 
+    """
     for ent in doc.ents:
         if ent.label_ == "PER":  # Personne
             print("Nom de personne :", ent.text)
         elif ent.label_ == "ORG":  # Organisation
             print("Nom d'organisation :", ent.text)
+    
+    """
+    for ent in doc.ents:
+        if ent.label_ == "PER" and ent.text not in people.keys():  # Personne
+            people[ent.text] = 1 
+        elif ent.label_ == "PER" and ent.text in people.keys():
+            people[ent.text] = people[ent.text] +1
+        elif ent.label_ == "ORG" and ent.text not in orgs.keys():  # Organisation
+            orgs[ent.text] = 1
+        elif ent.label_ == "ORG" and ent.text in orgs.keys():
+            orgs[ent.text] = orgs[ent.text] + 1 
+
+    print(dict(sorted(orgs.items(), key=lambda x: x[1])))
